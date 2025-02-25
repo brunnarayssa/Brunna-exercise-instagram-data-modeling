64017f1369153c6b1d18924ddd35bcc511a7061b
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2025-02-25 18:44:26 +0000

    cambios

--- src/models.py
+++ src/models.py
@@ -1,34 +1,85 @@
-import os
-import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+db = SQLAlchemy()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+    posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
+    followers = db.relationship('Follower', 
+                                foreign_keys='Follower.user_to_id',
+                                backref='followed', 
+                                lazy=True)
+    following = db.relationship('Follower', 
+                                foreign_keys='Follower.user_from_id',
+                                backref='follower', 
+                                lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email
+        }
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+class Follower(db.Model):
+    __tablename__ = 'follower'
+    id = db.Column(db.Integer, primary_key=True)
+    user_from_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    media = db.relationship('Media', backref='post', lazy=True)
+    comments = db.relationship('Comment', backref='post', lazy=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+        }
+
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Enum('image', 'video', name='media_type'), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id
+        }
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    comment_text = db.Column(db.String(500), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "post_id": self.post_id
+        }
