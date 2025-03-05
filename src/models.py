8aa12455a3072fb31cf01cd177dd9c4192954fd1
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2025-03-05 11:05:07 +0000

    guardandocambios

--- src/models.py
+++ src/models.py
@@ -1,85 +1,60 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
+class User(Base):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    firstname = db.Column(db.String(80), nullable=False)
-    lastname = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     
-    posts = db.relationship('Post', backref='author', lazy=True)
-    comments = db.relationship('Comment', backref='author', lazy=True)
-    followers = db.relationship('Follower', 
-                                foreign_keys='Follower.user_to_id',
-                                backref='followed', 
-                                lazy=True)
-    following = db.relationship('Follower', 
-                                foreign_keys='Follower.user_from_id',
-                                backref='follower', 
-                                lazy=True)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "firstname": self.firstname,
-            "lastname": self.lastname,
-            "email": self.email
-        }
-
-
-class Follower(db.Model):
+class Follower(Base):
     __tablename__ = 'follower'
-    id = db.Column(db.Integer, primary_key=True)
-    user_from_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-
-class Post(db.Model):
-    __tablename__ = 'post'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
-    media = db.relationship('Media', backref='post', lazy=True)
-    comments = db.relationship('Comment', backref='post', lazy=True)
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-        }
+    user_from = relationship(User, foreign_keys=[user_from_id])
+    user_to = relationship(User, foreign_keys=[user_to_id])
 
+class Post(Base):
+    __tablename__ = 'post'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
+    user = relationship(User)
 
-class Media(db.Model):
+class Media(Base):
     __tablename__ = 'media'
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.Enum('image', 'video', name='media_type'), nullable=False)
-    url = db.Column(db.String(200), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum("image", "video", name="media_type"), nullable=False)
+    url = Column(String(255), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "type": self.type,
-            "url": self.url,
-            "post_id": self.post_id
-        }
+    post = relationship(Post)
 
-
-class Comment(db.Model):
+class Comment(Base):
     __tablename__ = 'comment'
-    id = db.Column(db.Integer, primary_key=True)
-    comment_text = db.Column(db.String(500), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(255), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "comment_text": self.comment_text,
-            "author_id": self.author_id,
-            "post_id": self.post_id
-        }
+    author = relationship(User)
+    post = relationship(Post)
+
+# Generar la base de datos
+engine = create_engine('sqlite:///instagram.db')
+Base.metadata.create_all(engine)
